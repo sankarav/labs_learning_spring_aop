44d08682438d530409d243209e2affd964d8448d
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2016-01-23 13:42:46 -0500

    First push with the maven template created out of start.spring.io

---
+++ src/main/java/edu/ub/san/labs/LearnSpringAopApplication.java
@@ -0,0 +1,12 @@
+package edu.ub.san.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringAopApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/ub/san/labs/LearnSpringAopApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.ub.san.labs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LearnSpringAopApplication.class)
+public class LearnSpringAopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
